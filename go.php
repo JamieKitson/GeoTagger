8c2a9e45f46a58e89db1a8fc67483780009be75a
Jamie Kitson <jamiekitson@gmail.com>
2012-09-01 10:28:37 +0100

    use googleCall on go.php

--- go.php
+++ go.php
@@ -9,7 +9,6 @@ include('googleCall.php');
 if (!(testFlickr() && testLatitude()))
   header("Location: index.php");
 
-
 $fp = array(
     
     'sort' => 'date-taken-desc',
@@ -24,62 +23,35 @@ $fp = array(
 $fc = unserialize(flickrCall($fp));
 
 $photos = $fc['photos']['photo'];
+
 /*
-foreach ( $photos as $p)
-{
-echo $p['datetaken']."<br>\n";
-*/ 
+print_r($photos);
+usort($photos, function($a, $b) {$a['datetaken'] - $b['datetaken'];}); 
+print_r($photos);
+*/
 
 $first = (strtotime($photos[0]['datetaken']) + 24 * 60 * 60) * 1000;
 $last = (strtotime(end($photos)['datetaken']) - 24 * 60 * 60) * 1000;
 
-// print_r($fc);
-/*
-$count = 0;
-while ($count < 2) 
-{
-*/ 
-
-$opts = array(
-      'http'=>array(
-      'method'=>"GET",
-      'header'=>"Accept-Encoding: gzip\r\n" .
-                "User-Agent: my program (gzip)\r\n"
-                                )
-    );
-
-$context = stream_context_create($opts);
-
-//  echo date('c', $last / 1000)."<br>\n";
-// echo date('c', $first / 1000)."<br>\n";
-
  $photo = 0;
- $xmlresponse = true;
+ $locks= true;
  $count = 100;
- while (($count > 1) && ($xmlresponse !== FALSE))
+ while (($count > 1) && ($locks !== FALSE))
 {
-$url = 'https://www.googleapis.com/latitude/v1/location?oauth_token='.$_COOKIE[GOOGLE_TOKEN]."&granularity=best&max-results=1000&max-time=$first&min-time=$last";
 
-//echo $url."<br>\n";
+  list($locks, $count) = googleCall("max-results=1000&max-time=$first&min-time=$last");
 
-$xmlresponse = gzdecode(file_get_contents($url, false, $context));  
-//print_r($xmlresponse);
-  $locks = json_decode($xmlresponse);
-
-if ($xmlresponse !== FALSE)
+if ($count > 0)
 {
-  $locks = json_decode($xmlresponse);
-  if (property_exists($locks->data, 'items'))
-  {
-    $count = count($locks->data->items);
     $first = end($locks->data->items)->timestampMs;
-//    echo date('c', $first / 1000)." FIRST<br>\n";
   }
   else
     break;
 //  print_r($locks);
 //  $count = 0;
-}
+
+// go through photos
+
 $geo = 0;
 while (($d = strtotime($photos[$photo]['datetaken'])) > $first / 1000)
 {
@@ -101,31 +73,6 @@ while (($d = strtotime($photos[$photo]['datetaken'])) > $first / 1000)
 }
 
 
-/*
-elseif ($first - $last > 24 * 60 * 60 * 1000)
-{
-  break 2;
-}
-
-$first += 5 * 60 * 1000;
-$last -= 5 * 60 * 1000;
-}
-
-if ($xmlresponse === FALSE)
-  break;
-*/
-//echo $xmlresponse;
-//echo "\n\n<br><p>ab";
-
-
-//print_r($locks);
-//print_r($locks->data->items);
-/*
-foreach($locks->data->items as $loc)
-{
-  echo date('c', $loc->timestampMs / 1000)." ".$loc->latitude." ".$loc->longitude." "."<br>\n";
-} */
-//echo "<p>\n";
 }
 
 function geoLine($loc)
