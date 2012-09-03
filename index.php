50fe13d24ea729e6b0b13504ba4814d865c3df20
Jamie Kitson <jamiekitson@gmail.com>
2012-09-03 23:23:48 +0100

    ajax call, simplify html

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
   <title>Google Latitude to Flickr Geo-Tagger</title>
   <meta charset="UTF-8">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
   <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <style> #lblCount { display: inline } </style>
   <script src="bootstrap/js/bootstrap.min.js"></script>
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
   <script>
     $(document).ready(function(){
 
@@ -28,6 +29,29 @@
       $("#timezone").change(function(event){
         setCookie('timezone', $("#timezone").val(), 100);
       });
+
+      $('form').submit(function() {
+//        alert($(this).serialize());
+
+$.ajax({
+  url: "go.php?" + $(this).serialize(),
+  context: document.body
+}).done(function( data ) { 
+//alert(data);
+  $('#result').append(data);
+});
+
+
+        return false;
+      });
+
+/*      function saveValue(id) {
+        setCookie(id, $("#" + id).val(), 100);
+      }
+
+      function getValue(id, def) {
+        getCookie(id, $("#" + id).val(), def);
+      } */
 
     });
 
@@ -59,38 +83,33 @@ function getCookie(c_name, def)
 
 </head>
 <body>
-<div class="container">
-<div class="row-fluid">
-<div class="span2">
-</div>
-<div class="span10">
-<form action="go.php" class="form-horizontal">
-<div class="page-header">
-<h1>0. About</h1>
-</div>
+<form class="container">
+
+<h1 class="page-header">0. About</h1>
 <p>
 This app will attempt to automatically geo-tag your Flickr photos using your
 Google Latitude data. The exact location will be estimated by using the
 proportion of time passed between the two points either side of the time that
 the photo was taken. This app will never overwrite existing location data and
 will add a tag to any photos that it geo-tags, this might see like spam, but is
-there to help you in case anything should go wrong and cannot be disabled. You
+there to help you in case anything should go wrong and hence cannot be disabled. You
 can add search criteria in step 3 such as tags, sets, etc. By default this app
 will not write data back to Flickr but will show you what it will try to do and
 why.
 </p>
-</section>
-<div class="page-header">
-<h1 id="settz">1. Set your timezone</h1>
-</div>
+
+<h1 class="page-header" id="settz">1. Set your time zone</h1>
 <p>
 <a href="http://www.flickr.com/services/api/misc.dates.html">Flickr does not
-record the timezone</a> that your camera is set to, so to maximise accuracy it
-is very important to get the timezone right. Choosing your exact location
+record the time zone</a> that your camera is set to, so to maximise accuracy it
+is very important to get the time zone right. Choosing your exact location
 allows us to hopefully handle daylight saving. Note that this should match you
-camera setting, not just where you happen to be right now.
+camera setting, not just where you happen to be right now. Yes, I could attempt
+to get the time zone from the EXIF data, but from a brief and unscientific 
+survey it seems that this doesn't show up in Flickr for many cameras, my D7000
+being one of the few that does.
 </p>
-<select id="region">
+<select id="region" name="region">
 <option>Africa</option>
 <option>America</option>
 <option>Antarctica</option>
@@ -101,16 +120,17 @@ camera setting, not just where you happen to be right now.
 <option>Pacific</option>
 </select>
 
-<select id="timezone">
+<select id="timezone" name="timezone">
 </select>
 
-<h1 id="auth">2. Authenticate Google and Flickr</h1>
+<h1 class="page-header" id="auth">2. Authenticate Google and Flickr</h1>
 <p>
 The respective tokens will be held only as cookies in your browser and can be
 removed at any time by clicking the Disconnect button. The Goolge token has a
-fairly short life, so don't be suprised if you need to reauthenticate
+fairly short life, so don't be surprised if you need to re-authenticate
 regularly.
 </p>
+<p>
 <?php
 
 error_reporting(E_ALL);
@@ -123,16 +143,18 @@ $flickr = testFlickr();
 $latitude = testLatitude();
 
 if (!$flickr)
-  echo '<a class="btn btn-primary btn-large" href="getFlickr.php">Authorise Flickr</a>'."\n";
+  echo '<a class="btn btn-primary" href="getFlickr.php">Authorise Flickr</a>'."\n";
 
 if (!$latitude)
-  echo '<a class="btn btn-primary btn-large" href="https://accounts.google.com/o/oauth2/auth?client_id=60961182481.apps.googleusercontent.com&amp;redirect_uri='.baseHttpPath().'gotLatitude.php&amp;scope=https://www.googleapis.com/auth/latitude.all.best&amp;response_type=code">Authorise Google Latitude</a>'."\n";
+  echo '<a class="btn btn-primary" href="https://accounts.google.com/o/oauth2/auth?client_id=60961182481.apps.googleusercontent.com&amp;redirect_uri='.baseHttpPath().'gotLatitude.php&amp;scope=https://www.googleapis.com/auth/latitude.all.best&amp;response_type=code">Authorise Google Latitude</a>'."\n";
 
 if ($flickr || $latitude)
-  echo '<a class="btn btn-primary btn-large" href="disconnect.php">Disconnect</a>'."\n";
+  echo '<a class="btn" href="disconnect.php">Disconnect</a>'."\n";
 
 ?>
-<h1>3. Flickr search criteria (optional)</h1>
+</p>
+
+<h1 class="page-header">3. Flickr search criteria (optional)</h1>
 <P>
 Here you can add any criteria that exist for 
 <code><a href="http://www.flickr.com/services/api/flickr.photos.search.html">flickr.photos.search</a></code>
@@ -146,46 +168,53 @@ could also be useful if you don't want to geo-tag public photos, see the
 previous link for possible values. Note that you cannot override <code>has_geo</code>, this
 app will never overwrite existing location data. Unfortunately
 <code>flickr.photos.search</code> does not seem to have a sets setting.
-</P>
 <pre>
 tags=mongolia,mongolrally
 privacy_filter=4
 sort=date-posted-asc
 </pre>
-<textarea rows=5 cols=40 id="criteria"></textarea>
+<textarea rows=5 cols=40 name="criteria"></textarea>
 
-<h1 id="go">4. Go</h1>
-This app will geo-tag a maximum 500 photos at a time.
-<div class="control-group">
-<label class="control-label" for="count">Number of pictures to tag</label>
-<div class="controls"><input type="text" id="count" value="10"></div>
-</div>
-<div class="control-group">
+<h1 class="page-header" id="go">4. Go</h1>
+<p>
+This app will geo-tag a maximum 500 photos at a time and will not write the data back to Flickr unless you check the checkbox below.
+</p>
+<p>
+<label class="control-label" for="count" id="lblCount">Number of pictures to tag: </label>
+<input type="text" name="count" id="count" value="10">
 <label class="checkbox">Write data to Flickr<input type="checkbox" name="write" value="true"></label>
-</div>
+</p>
 
-<?php
+<p>
+<input type="submit" value="Go" class="btn btn-primary btn-large"<?php
 
-if ($flickr && $latitude)
-  echo '<input type="submit" value="Go" class="btn btn-primary btn-large">';
+if (!($flickr && $latitude))
+  echo ' disabled="disabled" ';
 
+?>>
+</p>
 
-?>
-<h1 id="alt">5. Alternatives</h1>
+<h1 class="page-header">5. Results</h1>
+<p>
+The results will be presented here as a table, mainly of links. 
+</p>
+<div id="result"></div>
+
+<h1 class="page-header" id="alt">6. Alternatives</h1>
 <ul>
 <li><a href="http://latitude2flickr.be-q.com/">Latitude2Flickr</a> has a very nice
 interface, but I don't think does anything automatically, or even in batches.
 It also dynamically loads all of your photos so can be quite slow if you have a
 lot of photos and is a little buggy (currently it's returning an error on
 Google authentication). 
-<li>A few desktop non-FLickr apps are discussed in 
+<li>A few desktop non-Flickr apps are discussed in 
 <a href="http://www.quora.com/Is-there-a-way-to-geotag-photos-using-Google-Latitude-history">this thread</a>.
 </ul>
-<h1 id="source">6. Source code</h1>
-<a href="https://github.com/JamieKitson/GeoTagger">On GitHub</a>.
+
+<h1 class="page-header" id="source">7. Source code</h1>
+<a href="https://github.com/JamieKitson/GeoTagger">On GitHub</a>. This is also the place to 
+<a href="https://github.com/JamieKitson/GeoTagger/issues">report bugs</a>.
 </form>
-</div>
-</div>
 </body>
 </html>
 
