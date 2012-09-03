b9138398839b7868eb0699f5f5e48ebd13874759
Jamie Kitson <jamiekitson@gmail.com>
2012-09-03 23:22:19 +0100

    minor removing debug messages

--- getFlickr.php
+++ getFlickr.php
@@ -8,13 +8,10 @@ function getRequestToken()
   $params = Array();
   $params['oauth_callback'] = baseHttpPath().'gotFlickr.php';
   $rsp = flickrCall($params, 'oauth/request_token');
-  echo $rsp;
   parse_str($rsp, $q);
-  print_r($q);
-  if ($q['oauth_callback_confirmed'] != true)
-    exit("Flickr didn't return oauth_callback_confirmed true");
+  if (!array_key_exists('oauth_callback_confirmed', $q) || $q['oauth_callback_confirmed'] != true)
+    exit("Flickr didn't return oauth_callback_confirmed true: $rsp");
   $url = 'http://www.flickr.com/services/oauth/authorize?oauth_token='.$q['oauth_token'];
-  echo "$url\n".$q['oauth_token_secret'];
   setcookie(FLICKR_SECRET, $q['oauth_token_secret']);
   header("Location: $url");
 }
