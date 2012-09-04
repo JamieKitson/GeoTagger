30bec93bf9c4325dcbe478fd130dfda26e581553
Jamie Kitson <jamiekitson@gmail.com>
2012-09-04 15:04:45 +0100

    refactor and indent

--- go.php
+++ go.php
@@ -61,110 +61,115 @@ $last = (end($photos)['udatetaken'] - 24 * 60 * 60) * 1000;
 echo '<table class="table">';
 echo "<tr><th>#</th><th>Flickr Photo</th><th>Prior Point</th><th>Next Point</th><th>Best Guess</th>".
   "<th>Tag</th><th>Geo</th></tr>";
- $photo = 0;
- $locks= true;
- $count = 100;
- while (($count > 1) && ($locks !== FALSE))
+$photo = 0;
+$locks= true;
+$count = 100;
+while (($count > 1) && ($locks !== FALSE))
 {
 
 //echo date('c')." BEFORE GOOGLE<br>\n";
   list($locks, $count) = googleCall("max-results=1000&max-time=$first&min-time=$last");
 //echo date('c')." $count AFTER GOOGLE<br>\n";
 
-flush();
+  flush();
 
-if ($count > 0)
-{
+  if ($count > 0)
+  {
     $first = end($locks->data->items)->timestampMs;
   }
   else
-{
+  {
     echo '</table><div class="alert alert-error">No data returned from latitude for '.
       formatDate($last / 1000).' to '.formatDate($first / 1000).'.</div>';
     break;
-}
-//  print_r($locks);
-//  $count = 0;
-
-// go through photos
-$geo = 0;
-while (($d = $photos[$photo]['udatetaken']) > $first / 1000)
-{
-  while (($geo < count($locks->data->items)) && ($d < $locks->data->items[$geo]->timestampMs / 1000))
-  {
-    $geo++;
   }
-  // all geo points are before photo
-  if ($d < $locks->data->items[$geo]->timestampMs / 1000)
-    break;
-  // if we have a photo before any geo data we can't do anything about it
-  if ($geo == 0)
+  //  print_r($locks);
+  //  $count = 0;
+
+  // go through photos
+  $geo = 0;
+  while (($d = $photos[$photo]['udatetaken']) > $first / 1000)
   {
-    echo "No data for photo ".$photos[$photo]['datetaken']." ".$photos[$photo]['id']."<br>\n";
-    $photo++;
-    continue;
-  }
-//  echo date('c', $locks->data->items[$geo - 1]->timestampMs / 1000)."<br>\n";
-
-  $prior = $locks->data->items[$geo - 1];
-  $next = $locks->data->items[$geo];
-
-  $id = $photos[$photo]['id'];
-  $title = /*utf8_decode(*/$photos[$photo]['title'];//);
-  if ($title == "")
-    $title = $id;
-  
-  echo "<tr><td>".($photo + 1)."</td><td><a href=\"http://flickr.com/photos/jamiekitson/$id\">$title</a></td>\n";
-  geoLine($next);
-  geoLine($prior);
-  
-  $dTime = ($prior->timestampMs - $next->timestampMs) / 1000; 
-  $dLat = $prior->latitude - $next->latitude;
-  $dLong = $prior->longitude - $next->longitude;
-
-  $multi = ($photos[$photo]['udatetaken'] - $prior->timestampMs / 1000) / $dTime; 
-  $lat = $multi * $dLat + $prior->latitude;
-  $long = $multi * $dLong + $prior->longitude;
-  latLine($lat, $long, $photos[$photo]['udatetaken']);
-
-  if (array_key_exists('write', $_GET) && ($_GET['write'] == true))
-  {
-    $rsp = flickrCall(array(
-          'method' => 'flickr.photos.addTags', 
-          'photo_id' => $id, 
-          'tags' => 'geotaggedfromlatitude'));
-    $p = unserialize($rsp);
-    if ($p['stat'] == 'ok')
+    while (($geo < count($locks->data->items)) && ($d < $locks->data->items[$geo]->timestampMs / 1000))
     {
-      echo "<td>Y</td>";
+      $geo++;
+    }
+    // all geo points are before photo
+    if ($d < $locks->data->items[$geo]->timestampMs / 1000)
+      break;
+    // if we have a photo before any geo data we can't do anything about it
+    if ($geo == 0)
+    {
+      echo "No data for photo ".$photos[$photo]['datetaken']." ".$photos[$photo]['id']."<br>\n";
+      $photo++;
+      continue;
+    }
+  //  echo date('c', $locks->data->items[$geo - 1]->timestampMs / 1000)."<br>\n";
+
+    $prior = $locks->data->items[$geo - 1];
+    $next = $locks->data->items[$geo];
+
+    $id = $photos[$photo]['id'];
+    $title = /*utf8_decode(*/$photos[$photo]['title'];//);
+    if ($title == "")
+      $title = $id;
+    
+    echo "<tr><td>".($photo + 1)."</td><td><a href=\"http://flickr.com/photos/jamiekitson/$id\">$title</a></td>\n";
+    geoLine($next);
+    geoLine($prior);
+    
+    $dTime = ($prior->timestampMs - $next->timestampMs) / 1000; 
+    $dLat = $prior->latitude - $next->latitude;
+    $dLong = $prior->longitude - $next->longitude;
+
+    $multi = ($photos[$photo]['udatetaken'] - $prior->timestampMs / 1000) / $dTime; 
+    $lat = $multi * $dLat + $prior->latitude;
+    $long = $multi * $dLong + $prior->longitude;
+    latLine($lat, $long, $photos[$photo]['udatetaken']);
+
+    if (array_key_exists('write', $_GET) && ($_GET['write'] == true))
+    {
       $rsp = flickrCall(array(
-            'method' => 'flickr.photos.geo.setLocation', 
+            'method' => 'flickr.photos.addTags', 
             'photo_id' => $id, 
-            'lat' => $lat, 
-            'lon' => $long));
-      $p = unserialize($rsp);
-      if ($p['stat'] == 'ok')
-        echo "<td>Y</td>";
+            'tags' => 'geotaggedfromlatitude'));
+      if (statToMessage($rsp))
+      {
+        $rsp = flickrCall(array(
+              'method' => 'flickr.photos.geo.setLocation', 
+              'photo_id' => $id, 
+              'lat' => $lat, 
+              'lon' => $long));
+        statToMessage($rsp);
+      }
       else
-        echo "<td>".$p['message']."</td>";
+        echo "<td>N</td>";
     }
     else
-      echo "<td>".$p['message']."</td><td>N</td>";
-  }
-  else
-    echo "<td>N</td><td>N</td>";
+      echo "<td>N</td><td>N</td>";
 
-  echo "</tr>\n";
-  $photo++;
-  flush();
-  if ($photo == count($photos))
-  {
-//echo date('c')." END <br>\n";
-echo "</table>";
-    exit;
+    echo "</tr>\n";
+    $photo++;
+    flush();
+    if ($photo == count($photos))
+    {
+      echo "</table>";
+      exit;
+    }
   }
+
 }
 
+function statToMessage($rsp)
+{
+  $p = unserialize($rsp);
+  if ($p['stat'] == 'ok')
+  {
+    echo "<td>Y</td>";
+    return true;
+  }
+  echo "<td>".$p['message']."</td>";
+  return false;
 
 }
 
