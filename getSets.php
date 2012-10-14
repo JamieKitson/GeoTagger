006d0645f288a0f71bd432a51ab01c29853c428a
Jamie Kitson <jamiekitson@gmail.com>
2012-10-14 18:00:07 +0100

    forgot to commit getSets.php #24

---
+++ getSets.php
@@ -0,0 +1,23 @@
+<?php
+
+include('flickrCall.php');
+
+function listFlickrSets()
+{
+  $res = "";
+  $params = array('method' => 'flickr.photosets.getList');
+  $rsp = flickrCall($params);
+  $fc = unserialize($rsp);
+  if ($fc['stat'] == 'ok')
+  {
+    foreach($fc['photosets']['photoset'] as $set)
+    {
+      $res .= '<option value="'.$set['id'].'">'.htmlentities($set['title']['_content']).'</option>';
+    }
+  }
+  return $res;
+}
+
+echo listFlickrSets();
+
+?>
